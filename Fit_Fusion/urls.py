852b3110bcfa0fb12732bd9f3a58a8dc12ff4891
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-05-05 19:01:42 +0545

    conflicts in urls

--- Fit_Fusion/urls.py
+++ Fit_Fusion/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
-=======
 from django.conf import settings
 from django.conf.urls.static import static
->>>>>>> ce4070e994bcf44a67b4954f1d02499cfb9cff96
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
     path("users/",include("Users.urls")),
-=======
     path("", include('home.urls')),
->>>>>>> ce4070e994bcf44a67b4954f1d02499cfb9cff96
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
