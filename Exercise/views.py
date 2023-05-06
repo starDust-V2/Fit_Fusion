6db1667f1c1f964aaee83afee461e04e8ffc2894
Prabin Bohara <prabinbohara10@gmail.com>
2023-05-07 03:23:27 +0545

    get_score api

--- Exercise/views.py
+++ Exercise/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .models import DailyScore
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,6 @@ def index(request):
 def individual_exercise(request):
 
     return render(request, 'Exercise/Exercise1.html')
+
+def get_score(request):
+    return JsonResponse(list(DailyScore.objects.all().values()), safe=False)
