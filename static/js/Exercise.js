87ad58f767086be56f7143ee43c6cadf96b3ea1a
Prabin Bohara <prabinbohara10@gmail.com>
2023-05-07 02:46:17 +0545

    3 exercise done

--- static/js/Exercise.js
+++ static/js/Exercise.js
@@ -19,13 +19,40 @@ async function api_call_all()
 		console.log(parsed_result[0].name);
 		
 		//displaying all the exercise:
+		var exerciseCard = `<div class="exercise-card">
+								<div class="name"><a href= 'individual_exercise?exercise_id=0662+'>push-up</a></div>
+								<div class="body-part"><span class="what-title"> Body-Part : &nbsp</span>chest</div>
+								<div class="equipment"><span class="what-title">Equipment : &nbsp</span>body weight</div>
+								<div class="target"><span class="what-title">Target Muscle : &nbsp</span>pectorals</div>
+								<img src='http://d205bpvrqc9yn1.cloudfront.net/0662.gif' alt="">
+								</div>` 
+
+			exerciseCard = exerciseCard + `<div class="exercise-card">
+								<div class="name"><a href= 'individual_exercise?exercise_id=3222+'>semi squat jump (male)</a></div>
+								<div class="body-part"><span class="what-title"> Body-Part : &nbsp</span>cardio</div>
+								<div class="equipment"><span class="what-title">Equipment : &nbsp</span>body weight</div>
+								<div class="target"><span class="what-title">Target Muscle : &nbsp</span>cardiovascular system</div>
+								<img src='http://d205bpvrqc9yn1.cloudfront.net/3222.gif' alt="">
+								</div>` 
+	
+			exerciseCard = exerciseCard + `<div class="exercise-card">
+								<div class="name"><a href= 'individual_exercise?exercise_id=1670+'>dumbbell one arm standing curl</a></div>
+								<div class="body-part"><span class="what-title"> Body-Part : &nbsp</span>upper arms</div>
+								<div class="equipment"><span class="what-title">Equipment : &nbsp</span>dumbbell</div>
+								<div class="target"><span class="what-title">Target Muscle : &nbsp</span>biceps	</div>
+								<img src= 'http://d205bpvrqc9yn1.cloudfront.net/1670.gif' alt="">
+								</div>`
+			document.querySelector(".main-content-inner").insertAdjacentHTML('beforeend', exerciseCard);
+			exerciseCard = "";
 		// Loop through exercises and generate HTML dynamically
 		 for (var i = 0; i < 100; i++) {
             var exercise = parsed_result[i];
 			var exercise_id = exercise.id;
-			console.log(exercise_id)
+			console.log(exercise_id);
 
-			var exerciseCard = `<div class="exercise-card">
+	
+
+			exerciseCard = `<div class="exercise-card">
 								<div class="name"><a href= 'individual_exercise?exercise_id=${exercise_id}+'>${exercise.name}</a></div>
 								<div class="body-part"><span class="what-title"> Body-Part : &nbsp</span>${exercise.bodyPart}</div>
 								<div class="equipment"><span class="what-title">Equipment : &nbsp</span>${exercise.equipment}</div>
@@ -34,7 +61,7 @@ async function api_call_all()
 								</div>` 
 
 
-			document.querySelector(".main-content-inner").insertAdjacentHTML('beforeend', exerciseCard)
+			document.querySelector(".main-content-inner").insertAdjacentHTML('beforeend', exerciseCard);
 		 }
 
 	} catch (error) {
