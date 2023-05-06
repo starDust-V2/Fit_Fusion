d27638937aa7dc378453c1fd49f338b1e1d62b33
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-05-06 19:15:46 +0545

    post view updated

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
 
 # Create your views here.
@@ -66,25 +66,17 @@ def confessions(request):
     "image_url" : "one.png",
     "name" : "John Doe",
     "bio" : "This is the bio for me if there's any. Cause I need a one dance. Got a hennessey in my hands"
-}
+    }
     if request.method == "POST":
         content=request.POST["content"]
         author=request.user
-        post_type=0
         title=request.POST["title"]
         user_post=Post(title=title,author=author,content=content,post_type=0)
         user_post.save()
 
-    posts=Post.objects.filter(post_type=0)
+        return redirect('confessions')
     
-    # posts = [
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ["Title of the post","author1","2079 Jan 2 11:00pm", "Hello from the other side. I must have called a thousand times. To tell I'm sorry for everything that I have done. Hello from the outside. I need one dnace got a hennessey in my hands one more time for I go higher powers taking a hold on me ",1,1,{}],
-    # ]
+    posts=Post.objects.filter(post_type=0).reverse()
     context = {
         'link_position': 0,
         "css_url" : "css/home.css",
@@ -92,4 +84,5 @@ def confessions(request):
         "posts" : posts
 
     }
+ 
     return render(request,'home/confessions.html',context)
