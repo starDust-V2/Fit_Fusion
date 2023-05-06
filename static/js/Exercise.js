19b0ad9c336a81a254cdd59eeebd6d27a049e020
upoudel888 <upoudel888@gmail.com>
2023-05-06 10:37:40 +0545

    Exercise ko display vayo

--- static/js/Exercise.js
+++ static/js/Exercise.js
@@ -43,6 +43,45 @@ async function api_call_all()
 }
 
 
+async function api_call_individual()
+{
+    //get variable value from url:
+    const urlParams = new URLSearchParams(window.location.search);
+    const exercise_id = urlParams.get('exercise_id');
+
+    const url = 'https://exercisedb.p.rapidapi.com/exercises/exercise/'+ exercise_id ;
+    console.log(url);
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c56e9a8e6emshb21b889df59c1f3p1325f2jsn828ee3b47f1e',
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        let parsed_result = JSON.parse(result);
+        let exercise= parsed_result;
+
+        var html = "<div class='exercise-card'>";
+            html += "<h1>Exercise Details</h1>";
+            html += "<p><strong>Name:</strong> " + exercise.name + "</p>";
+            html += "<p><strong>Body Part:</strong> " + exercise.bodyPart + "</p>";
+            html += "<p><strong>Equipment:</strong> " + exercise.equipment + "</p>";
+            html += "<p><strong>Target:</strong> " + exercise.target + "</p>";
+            html += "<img src='" + exercise.gifUrl + "' alt='Exercise GIF'>";
+            html += "</div>";
+
+        document.getElementById('main_div').innerHTML= html;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 
 
 
